9deac3c42da6735fc0d54c8b651c6d14bdbac717
Guillaume Libersat <glibersat@sigill.org>
2021-10-19 22:08:08 +0200

    Global  - Add recogito lib  - Enhance style
    Document
     - Add tags

--- documents/models.py
+++ documents/models.py
@@ -1,11 +1,13 @@
 from urllib.parse import urljoin
 
 import markdown
+import nltk
 from bs4 import BeautifulSoup, Comment
 from django.db import models
 from django.utils import timezone
 from markdown_link_attr_modifier import LinkAttrModifierExtension
 from markdownify import markdownify
+from nltk.stem import WordNetLemmatizer
 
 
 class Document(models.Model):
@@ -20,7 +22,7 @@ class Document(models.Model):
         return soup.title.get_text()
 
     @property
-    def content_markdown(self):
+    def content_html_clean(self):
         soup = BeautifulSoup(self.content_raw, "lxml")
         soup = soup.find("body")
         for s in soup(
@@ -50,12 +52,42 @@ class Document(models.Model):
         for element in soup.findAll(text=lambda text: isinstance(text, Comment)):
             element.extract()
 
+        return soup
+
+    @property
+    def content_markdown(self):
         return markdown.markdown(
-            markdownify(str(soup)),
+            markdownify(str(self.content_html_clean)),
             extensions=[
                 LinkAttrModifierExtension(new_tab="on", no_referrer="external_only")
             ],
         )
+
+    @property
+    def tags(self):
+        html = self.content_html_clean
+        text = html.findAll(text=True)
+
+        tokens = nltk.tokenize.word_tokenize(str(text))
+        allWords = [word for word in tokens if word.isalpha()]
+        stopwords = nltk.corpus.stopwords.words("english")
+
+        allWordExceptStopDist = nltk.FreqDist(
+            w.lower() for w in allWords if w.lower() not in stopwords
+        )
+
+        # XXX Could use snowball stemmer here (multi language)
+        lemmatizer = WordNetLemmatizer()
+
+        tags = set([])
+        for word in allWordExceptStopDist.most_common(15):
+            if len(word[0]) > 2:
+                tags.add(lemmatizer.lemmatize(word[0]))
+
+            if len(tags) >= 5:
+                break
+
+        return tags
 
     def __str__(self):
         return self.title
